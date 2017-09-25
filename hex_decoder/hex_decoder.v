f6a9e8c1ccb0c20301746021738eeee4ff01d2c2
dymnz <vicodin1123@gmail.com>
2017-09-25 21:19:48 +0800

    'hex'

--- hex_decoder/hex_decoder.v
+++ hex_decoder/hex_decoder.v
@@ -8,7 +8,7 @@ wire c = SW[2];
 wire d = SW[3];
 
 assign HEX0[0] = ~(~d&b | ~c&~b&~a | d&~c&~b | ~d&c&a);
-assign HEX0[1] = ~(~d&~b | ~c&~b&~a | ~d&c&~a | d&~c&~b);
+assign HEX0[1] = ~(~d&b | ~d&~c | ~d&~a | ~c&~b);
 assign HEX0[2] = ~(~c&~b | ~d&~b | ~d&a | ~d&c);
 assign HEX0[3] = ~(~c&~b&~a | ~d&c&~b&a | ~d&~c&b | ~d&b&~a);
 assign HEX0[4] = ~(~c&~b&~a | ~d&b&~a);
