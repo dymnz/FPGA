87a979cdc92a1103a1d60635ffe67e095e5243e6
dymnz <vicodin1123@gmail.com>
2017-10-06 21:02:33 +0800

    yo

--- lab_02/02_07_RTC/Hour_module.v
+++ lab_02/02_07_RTC/Hour_module.v
@@ -1,67 +1,26 @@
 /*
-	CLK: posedge increment
+	CLK: CARRY of Minute_module
 	RST: posedge reset
+	COUNT_SET: COUNT to change to when resetting
 	COUNT: 0 ~ 23
-	CARRY: 0 -> 1 when COUNT 59 -> 0
 
-	Can be used in second or minute part of the RTC
-
-	test passed 
+	not tested
 */
-module Hour_module (CLK, RST, COUNT, CARRY);
+module Hour_module (CLK, COUNT_SET, RST, COUNT);
 
 	input CLK, RST;
-	output reg CARRY;
+	input [5:0] COUNT_SET;
 	output reg [5:0] COUNT;
 	
-	initial CARRY = 0;
 	initial COUNT = 0;
 	
 	always @ (posedge CLK or posedge RST) begin
 		if (RST) begin
-			COUNT <= 0;
-		end else if (COUNT == 0) begin
+			COUNT <= COUNT_SET;
+		end else if (COUNT < 24) begin
 			COUNT <= COUNT + 1;
-			CARRY <= 0;
-		end else if (COUNT < 23) begin
-			COUNT <= COUNT + 1;
-		end else if (COUNT == 23) begin
+		end else if (COUNT == 24) begin
 			COUNT <= 0;
-			CARRY <= 1;
 		end		
 	end
 endmodule
-
-////////////////////////////////////////////////
-
-module Hour_module_tb ();
-	reg CLK, RST;
-	wire CARRY;
-	wire [5:0] COUNT;
-	
-	Hour_module DUT (
-		.CLK(CLK), 
-		.RST(RST), 
-		.COUNT(COUNT), 
-		.CARRY(CARRY));
-	
-	initial begin	
-		CLK = 0;
-		forever begin
-			#5	CLK = ~CLK;
-		end
-		#1000	$finish;
-	end
-	
-	initial begin
-		RST = 0;
-		#100	RST = 1;
-		#5 	RST = 0;
-	end
-	
-	initial begin
-		#1000	$finish;
-	end
-	
-endmodule
-	
---
+++ lab_02/02_07_RTC/RTC.v
@@ -0,0 +1,44 @@
+module RTC(SW, CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+
+	input CLOCK_50;
+	input [9:0] SW;
+	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	wire [3:0] HEX_input [5:0];
+	
+	wire set_hour, rst;
+	wire hour_rst, min_rst;
+	wire clk_1hz;
+	wire [3:0] BCD_0, BCD_1;
+	wire [5:0] count_set;
+	wire [5:0] hour_count, min_count, sec_count;
+	
+	wire sec_carry, min_carry;
+	
+	assign BCD_0 = SW[3:0];
+	assign BCD_1 = SW[7:4];
+	assign rst = SW[8];
+	assign set_hour = SW[9];
+	
+	assign hour_rst = set_hour & rst;
+	assign min_rst = ~set_hour & rst;
+	
+	assign count_set = BCD_1 * 10 + BCD_0;
+	
+	One_hertz_module One_hertz(CLOCK_50, rst, clk_1hz);
+	
+	Second_module S_m(clk_1hz, rst, sec_count, sec_carry);
+	Minute_module M_m(sec_carry, count_set, min_rst, min_count, min_carry);
+	Hour_module H_m(min_carry, count_set, hour_rst, hour_count);
+	
+	Bin2BCD S_BCD(sec_count, HEX_input[0], HEX_input[1]);
+	Bin2BCD M_BCD(min_count, HEX_input[2], HEX_input[3]);
+	Bin2BCD H_BCD(hour_count, HEX_input[4], HEX_input[5]);
+	
+	hex_decoder_top h_0(HEX_input[0], HEX0);
+	hex_decoder_top h_1(HEX_input[1], HEX1);
+	hex_decoder_top h_2(HEX_input[2], HEX2);
+	hex_decoder_top h_3(HEX_input[3], HEX3);
+	hex_decoder_top h_4(HEX_input[4], HEX4);
+	hex_decoder_top h_5(HEX_input[5], HEX5);
+
+endmodule
